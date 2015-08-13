c8a5cc31aa669d4c1f0ba2b7e189ef15b46d1ed1
Matthew Mayer <matthewkmayer@gmail.com>
2015-08-13 15:34:09 -0700

    Updates main to stop throwing compiler warnings, we're still in S3 debug mode.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,7 +10,6 @@ use rusoto::sqs::*;
 use rusoto::s3::*;
 use time::*;
 
-
 fn main() {
 	let mut provider = DefaultAWSCredentialsProviderChain::new();
 	let creds = provider.get_credentials();
@@ -38,44 +37,48 @@ fn get_buckets_test(creds: &AWSCredentials) -> Result<(), AWSError> {
 	}
 
 	println!("Done calling s3.");
-	panic!("boop");
+	panic!("Not implemented.");
 	// Ok(());
 }
 
 fn sqs_roundtrip_tests(creds: &AWSCredentials) -> Result<(), AWSError> {
-	// let sqs = SQSHelper::new(&creds, "us-east-1");
-	//
-	// // list existing queues
-	// let response = try!(sqs.list_queues());
-	// for q in response.queue_urls {
-	// 	println!("Existing queue: {}", q);
-	// }
-	//
-	// // create a new queue
-	// let q_name = &format!("test_q_{}", get_time().sec);
-	// let response = try!(sqs.create_queue(q_name));
-	// println!("Created queue {} with url {}", q_name, response.queue_url);
-	//
-	// // query it by name
-	// let response = try!(sqs.get_queue_url(q_name));
-	// let queue_url = &response.queue_url;
-	// println!("Verified queue url {} for queue name {}", queue_url, q_name);
-	//
-	// // send it a message
-	// let msg_str = "lorem ipsum dolor sit amet";
-	// let response = try!(sqs.send_message(queue_url, msg_str));
-	// println!("Send message with body '{}' and created message_id {}", msg_str, response.message_id);
-	//
-	// // receive a message
-	// let response = try!(sqs.receive_message(queue_url));
-	// for msg in response.messages {
-	// 	println!("Received message '{}' with id {}", msg.body, msg.message_id);
-	// 	try!(sqs.delete_message(queue_url, &msg.receipt_handle));
-	// }
-	//
-	// // delete the queue
-	// try!(sqs.delete_queue(queue_url));
-	// println!("Queue {} deleted", queue_url);
+	// Matt's debugging
+	if true {
+		return Ok(());
+	}
+	let sqs = SQSHelper::new(&creds, "us-east-1");
+
+	// list existing queues
+	let response = try!(sqs.list_queues());
+	for q in response.queue_urls {
+		println!("Existing queue: {}", q);
+	}
+
+	// create a new queue
+	let q_name = &format!("test_q_{}", get_time().sec);
+	let response = try!(sqs.create_queue(q_name));
+	println!("Created queue {} with url {}", q_name, response.queue_url);
+
+	// query it by name
+	let response = try!(sqs.get_queue_url(q_name));
+	let queue_url = &response.queue_url;
+	println!("Verified queue url {} for queue name {}", queue_url, q_name);
+
+	// send it a message
+	let msg_str = "lorem ipsum dolor sit amet";
+	let response = try!(sqs.send_message(queue_url, msg_str));
+	println!("Send message with body '{}' and created message_id {}", msg_str, response.message_id);
+
+	// receive a message
+	let response = try!(sqs.receive_message(queue_url));
+	for msg in response.messages {
+		println!("Received message '{}' with id {}", msg.body, msg.message_id);
+		try!(sqs.delete_message(queue_url, &msg.receipt_handle));
+	}
+
+	// delete the queue
+	try!(sqs.delete_queue(queue_url));
+	println!("Queue {} deleted", queue_url);
 
 	Ok(())
 }
