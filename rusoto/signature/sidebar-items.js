0c43688bcbc473b6e352597e2e251835371517e5
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-11 20:15:17 -0800

    Doc push.

--- rusoto/signature/sidebar-items.js
+++ rusoto/signature/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["string_to_sign",""]],"struct":[["SignedRequest","A data structure for all the elements of an HTTP request that are involved in the Amazon Signature Version 4 signing process"]]});
+initSidebarItems({"fn":[["string_to_sign","Mark string as AWS4-HMAC-SHA256 hashed"]],"struct":[["SignedRequest","A data structure for all the elements of an HTTP request that are involved in the Amazon Signature Version 4 signing process"]]});
